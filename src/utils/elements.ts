8f5dec4a345b3ce7aba5fc18a34bb8dc195cb1c6
lucas <lpatronr@gmail.com>
2022-07-06 20:46:55 -0500

    feat: add DOM elements

--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -1,7 +1,52 @@
 function getElement(query: string): Element {
   const el = document.querySelector(query);
-  if (!el) throw new Error(`Element not found: ${query}`);
+  if (!el) {
+    alert(`Element not found: ${query}`);
+    throw new Error(`Element not found: ${query}`);
+  }
   return el;
 }
 
-export const saveButton = getElement('#save-button') as HTMLButtonElement;
+function getAllElements(query: string): NodeList {
+  const els = document.querySelectorAll(query);
+  if (!els.length) {
+    alert(`No elements found: ${query}`);
+    throw new Error(`No elements found: ${query}`);
+  }
+  return els;
+}
+
+export function getAllListItems(): NodeList {
+  return getAllElements('li');
+}
+
+// SAVE ALL MODAL
+const saveButton = getElement('#save-all') as HTMLButtonElement;
+const saveAllModal = getElement('#save-all-modal') as HTMLDivElement;
+const saveAllModalClose = getElement('#save-all-modal-close') as HTMLButtonElement;
+
+// CONTAINERS
+const backlogContainer = getElement('#backlog-container') as HTMLUListElement;
+const wipContainer = getElement('#wip-container') as HTMLUListElement;
+const inReviewContainer = getElement('#in-review-container') as HTMLUListElement;
+const finishedContainer = getElement('#finished-container') as HTMLUListElement;
+
+// ADD TASK MODAL
+const addTaskModalButton = getElement('#add-task-modal-button') as HTMLButtonElement;
+const addTaskModalForm = getElement('#add-task-modal-form') as HTMLFormElement;
+const addTaskModal = getElement('#add-task-modal') as HTMLDivElement;
+const addTaskModalClose = getElement('#add-task-modal-close') as HTMLButtonElement;
+
+export const elements = {
+  saveButton,
+  backlogContainer,
+  wipContainer,
+  inReviewContainer,
+  finishedContainer,
+  saveAllModal,
+  saveAllModalClose,
+  addTaskModalButton,
+  addTaskModalForm,
+  addTaskModal,
+  addTaskModalClose,
+};
