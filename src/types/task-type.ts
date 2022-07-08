ce5d45e4d60fdc68a43c52ddefa19878a294e9a2
lucas <lpatronr@gmail.com>
2022-07-08 02:02:51 -0500

    feat: add dueDate property

--- src/types/task-type.ts
+++ src/types/task-type.ts
@@ -4,6 +4,7 @@ export type TaskType = {
   description: string;
   comments: string[];
   date: string;
+  dueDate: string;
   priority: 'low' | 'medium' | 'high';
   status: 'backlog' | 'work-in-progress' | 'in-review' | 'finished';
 };
