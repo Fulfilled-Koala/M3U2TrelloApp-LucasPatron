bb091a27ba367f74d3b725b2a0fd94ff8a3f0e33
lucas <lpatronr@gmail.com>
2022-07-08 02:03:07 -0500

    feat: implement drag and drop functionality

--- src/utils/drag-and-drop.ts
+++ src/utils/drag-and-drop.ts
@@ -1,33 +1,71 @@
 import { setStatus } from '../api/set-status';
 import { elements, getAllListItems } from './elements';
 
-const containers = elements.containers;
-let draggedItem: null | Element = null;
+const containers: HTMLUListElement[] = [
+  elements.containers.backlogContainer,
+  elements.containers.wipContainer,
+  elements.containers.inReviewContainer,
+  elements.containers.finishedContainer,
+];
+let draggedItem: null | HTMLLIElement = null;
 let previousContainer: null | Element = null;
+
+function appendPlaceholder(container: HTMLUListElement) {
+  if (!container.querySelector('.placeholder') && !container.contains(draggedItem)) {
+    const placeholder = document.createElement('li');
+    placeholder.className =
+      'placeholder opacity-50 bg-white flex flex-col gap-2 rounded-lg p-2 pointer-events-none';
+    placeholder.insertAdjacentHTML('afterbegin', draggedItem.innerHTML);
+    container.appendChild(placeholder);
+  }
+}
+
+function handleDrop(container: HTMLUListElement) {
+  if (draggedItem) container.appendChild(draggedItem);
+  container.querySelector('.placeholder')?.remove();
+
+  if (previousContainer && previousContainer.childElementCount === 0) {
+    const li = document.createElement('li');
+    li.className = 'no-tasks-found text-center font-medium text-red-500';
+    li.textContent = 'No tasks found';
+    previousContainer.appendChild(li);
+  }
+
+  if (container.querySelector('.no-tasks-found')) {
+    container.querySelector('.no-tasks-found')?.remove();
+  }
+}
 
 export function setDraggable() {
   const listItems: NodeListOf<HTMLLIElement> = getAllListItems();
 
   // Add drag functionality for each list item
   for (const listItem of listItems) {
-    listItem.addEventListener('dragstart', () => {
+    listItem.ondragstart = () => {
       setTimeout(() => {
         previousContainer = listItem.parentElement;
 
         // Add the drop zone effect
         for (const container of containers) {
+          const dropZone = container.parentElement.querySelector('.drop-zone');
+          // Remove 'hidden' class from drop zone if the container is not the one the item is in
+          if (container !== listItem.parentElement) {
+            dropZone.classList.remove('hidden');
+          }
           container.classList.add('bg-blue-100');
           container.classList.remove('bg-slate-50');
+          for (const child of container.children) {
+            child.classList.add('pointer-events-none');
+          }
         }
 
         draggedItem = listItem;
         listItem.style.display = 'none';
       }, 0);
-    });
+    };
 
-    listItem.addEventListener('dragend', () => {
+    listItem.ondragend = () => {
       setTimeout(() => {
-        // Set the status to the corresponding container from the STORE
         const id = Number(listItem.id);
         const status = listItem.parentElement.id as
           | 'backlog'
@@ -36,46 +74,66 @@ export function setDraggable() {
           | 'finished';
         setStatus(id, status);
 
-        listItem.style.display = 'block';
+        listItem.style.display = 'flex';
         draggedItem = null;
 
         // Remove the drop zone effect
         for (const container of containers) {
+          const dropZone = container.parentElement.querySelector('.drop-zone');
+          dropZone.classList.add('hidden');
           container.classList.remove('bg-blue-100');
           container.classList.add('bg-slate-50');
+          for (const child of container.children) {
+            child.classList.remove('pointer-events-none');
+          }
         }
       }, 0);
-    });
+    };
   }
 
   // Add the drop functionality for each list
   for (const container of containers) {
-    container.addEventListener('dragover', (e) => {
-      e.preventDefault();
-      container.classList.add('shadow-lg');
-    });
+    const dropZone = container.parentElement.querySelector('.drop-zone') as HTMLDivElement;
 
-    container.addEventListener('dragenter', (e) => {
+    dropZone.ondragover = (e) => {
       e.preventDefault();
-    });
+      e.stopPropagation();
+      dropZone.classList.remove('animate-pulse', 'border-blue-500', 'bg-blue-50');
+      dropZone.classList.add('border-blue-600', 'bg-blue-100');
+      appendPlaceholder(container);
+    };
 
-    container.addEventListener('dragleave', (e) => {
+    dropZone.ondragleave = (e) => {
       e.preventDefault();
+      e.stopPropagation();
+      container.querySelector('.placeholder')?.remove();
+      dropZone.classList.remove('border-blue-600', 'bg-blue-100');
+      dropZone.classList.add('border-blue-500', 'bg-blue-50', 'animate-pulse');
+    };
+
+    dropZone.ondrop = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      handleDrop(container);
+    };
+
+    container.ondragover = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      appendPlaceholder(container);
+    };
+
+    container.ondragleave = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      container.querySelector('.placeholder')?.remove();
       container.classList.remove('shadow-lg');
-    });
+    };
 
-    container.addEventListener('drop', (e) => {
+    container.ondrop = (e) => {
+      e.stopPropagation();
       e.preventDefault();
-      if (draggedItem) container.appendChild(draggedItem);
-
-      const p = container.querySelector('p');
-      if (p && p.textContent === 'No tasks found') container.removeChild(p);
-
-      if (previousContainer && previousContainer.childElementCount === 0) {
-        const p = document.createElement('p');
-        p.innerHTML = `<p class='text-center font-medium text-red-500'>No tasks found</p>`;
-        previousContainer.appendChild(p);
-      }
-    });
+      handleDrop(container);
+    };
   }
 }
