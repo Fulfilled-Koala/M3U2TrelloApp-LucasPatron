8aa564d995ccd283b7c4eb1fc95da5f7f7dac5cd
lucas <lpatronr@gmail.com>
2022-07-08 02:00:34 -0500

    refactor: extract modal functionality to their respective modules

--- src/modals/edit-task-modal.ts
+++ src/modals/edit-task-modal.ts
@@ -1,7 +1,35 @@
+import { editOne } from '../api/edit-one';
+import { TaskType } from '../types/task-type';
 import { elements } from '../utils/elements';
 
+const { closeButton, modal, inputs, title, submitButton } = elements.editTaskModal;
+
 export function setEditTaskModalListeners() {
-  elements.editTaskModal.closeButton.onclick = () => {
-    elements.editTaskModal.modal.classList.add('hidden');
+  closeButton.onclick = () => {
+    modal.classList.add('hidden');
+  };
+}
+
+export function setEditTaskModal(task: TaskType) {
+  const { description, priority, tags } = task;
+  modal.classList.remove('hidden');
+  title.innerHTML = `<span class='text-black text-xs'>CURRENTLY EDITING:</span> ${description}`;
+  inputs.description.value = description;
+  inputs.priority.value = priority;
+  inputs.tags.value = tags.join(',');
+
+  submitButton.onclick = () => {
+    if (!['high', 'medium', 'low'].includes(inputs.priority.value)) {
+      return alert('Priority must be high, medium, or low');
+    }
+
+    const newTask: TaskType = {
+      ...task,
+      description: inputs.description.value,
+      priority: inputs.priority.value as 'low' | 'medium' | 'high',
+      tags: inputs.tags.value.split(','),
+    };
+
+    editOne(newTask);
   };
 }
--- src/modals/save-all-modal.ts
+++ src/modals/save-all-modal.ts
@@ -2,13 +2,15 @@ import { STORE } from '../api/store';
 import { LOCAL_STORAGE_KEY } from '../constants/local-storage';
 import { elements } from '../utils/elements';
 
+const { closeButton, modal, openButton } = elements.saveAllModal;
+
 export function setSaveAllModalListeners() {
-  elements.saveButton.onclick = () => {
-    elements.saveAllModal.classList.remove('hidden');
+  openButton.onclick = () => {
+    modal.classList.remove('hidden');
     window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(STORE));
   };
 
-  elements.saveAllModalClose.onclick = () => {
-    elements.saveAllModal.classList.add('hidden');
+  closeButton.onclick = () => {
+    modal.classList.add('hidden');
   };
 }
