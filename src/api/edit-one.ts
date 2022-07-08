2670b690e27e03c7fd48f6525a79b1e9b4d5bcfc
lucas <lpatronr@gmail.com>
2022-07-08 02:01:46 -0500

    feat: add functionality for editing a single task

---
+++ src/api/edit-one.ts
@@ -0,0 +1,11 @@
+import { LOCAL_STORAGE_KEY } from '../constants/local-storage';
+import { TaskType } from '../types/task-type';
+import { render } from '../utils/render-tasks';
+import { STORE } from './store';
+
+export function editOne(task: TaskType): void {
+  const index = STORE.findIndex(({ id }) => id === task.id);
+  STORE[index] = task;
+  window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(STORE));
+  render();
+}
