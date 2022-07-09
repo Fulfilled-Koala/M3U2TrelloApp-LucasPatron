a40a5d661ffc2d915082b8cda6bce9146b161744
lucas <lpatronr@gmail.com>
2022-07-09 00:33:59 -0500

    feat: add local storage validation

--- src/api/store.ts
+++ src/api/store.ts
@@ -8,7 +8,34 @@ export const STORE: TaskType[] = [];
 export function initializeStore(): void {
   const tasks = window.localStorage.getItem(LOCAL_STORAGE_KEY);
   if (tasks) {
-    STORE.push(...JSON.parse(tasks));
-    render();
+    if (!Array.isArray(JSON.parse(tasks))) return alert('Invalid data in local storage');
+    const tasksArray = JSON.parse(tasks) as unknown[];
+
+    if (
+      !tasksArray.every((task: unknown) => {
+        console.log(task instanceof Object && task.constructor === Object);
+        return task instanceof Object && task.constructor === Object;
+      })
+    )
+      return alert('Invalid data in local storage');
+
+    if (
+      !tasksArray.every((task: { [key: string]: unknown }) => {
+        return (
+          task.id &&
+          task.tags &&
+          task.description &&
+          task.comments &&
+          task.date &&
+          task.dueDate &&
+          task.priority &&
+          task.status
+        );
+      })
+    )
+      return alert('Invalid data in local storage');
+
+    STORE.push(...(tasksArray as TaskType[]));
   }
+  render();
 }
