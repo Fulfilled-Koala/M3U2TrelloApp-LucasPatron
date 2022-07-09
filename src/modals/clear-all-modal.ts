8f3092a3f6519592b58ee1268f731e95345bec74
lucas <lpatronr@gmail.com>
2022-07-09 00:33:40 -0500

    feat: change from save all to remove all of the existing tasks

---
+++ src/modals/clear-all-modal.ts
@@ -0,0 +1,19 @@
+import { elements } from '../utils/elements';
+import { LOCAL_STORAGE_KEY } from '../constants/local-storage';
+import { render } from '../utils/render-tasks';
+import { STORE } from '../api/store';
+
+const { closeButton, modal, openButton } = elements.clearAllModal;
+
+export function setClearAllModalListeners() {
+  openButton.onclick = () => {
+    modal.classList.remove('hidden');
+    window.localStorage.removeItem(LOCAL_STORAGE_KEY);
+    STORE.length = 0;
+    render();
+  };
+
+  closeButton.onclick = () => {
+    modal.classList.add('hidden');
+  };
+}
