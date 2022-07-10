b7b4623b74b4eaae8617ad01d39b4947dc7e1cdc
lucas <lpatronr@gmail.com>
2022-07-10 02:05:38 -0500

    refactor: one-liner return statement

--- src/api/get-all.ts
+++ src/api/get-all.ts
@@ -4,14 +4,18 @@ import showSuccessToast from '../toasts/success';
 import showErrorToast from '../toasts/error';
 
 export async function httpGetAll(): Promise<TaskType[]> {
-  const response = await axios.get(process.env.API_ROOT);
-  if (response.status === 200) {
-    showSuccessToast('Successfully fetched all tasks');
-    const tasks = response.data.tasks as TaskType[];
-    return tasks;
-  } else {
+  try {
+    const response = await axios.get(process.env.API_ROOT);
+    if (response.status === 200) {
+      showSuccessToast('Successfully fetched all tasks');
+      return response.data.tasks as TaskType[];
+    }
+
     showErrorToast('Failed to fetch all tasks');
     console.error(response.statusText);
     return Promise.reject(response.statusText);
+  } catch (e) {
+    showErrorToast('Failed to fetch all tasks');
+    console.error(e);
   }
 }
