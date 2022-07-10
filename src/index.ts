7da8c700513207c2659ef4037649cd6f435ad9e0
lucas <lpatronr@gmail.com>
2022-07-10 02:03:35 -0500

    feat: enable toggling between dark and light modes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './style.css';
 import initializeStore from './store';
 import initializeEventListeners from './modals';
+import initializeTheme from './utils/theme';
 
 initializeStore();
 initializeEventListeners();
+initializeTheme();
