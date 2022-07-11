da70e535e4f33ea1cf478f426da2bd6fee4539f9
lucas <lpatronr@gmail.com>
2022-07-10 20:26:07 -0500

    feat: allow editing dueDate

--- src/modals/edit-task-modal.ts
+++ src/modals/edit-task-modal.ts
@@ -1,5 +1,6 @@
 import httpDeleteOne from '../api/delete-one';
 import httpPatchOne from '../api/patch-one';
+import showErrorToast from '../toasts/error';
 import { TaskType } from '../types/task-type';
 import { elements } from '../utils/elements';
 
@@ -12,23 +13,25 @@ export function setEditTaskModalListeners() {
 }
 
 export function setEditTaskModal(task: TaskType) {
-  const { description, priority, tag } = task;
+  const { description, priority, tag, dueDate } = task;
   modal.classList.remove('hidden');
   title.innerHTML = `<span class='text-black text-xs dark:text-white'>CURRENTLY EDITING:</span> ${description}`;
   inputs.description.value = description;
   inputs.priority.value = priority;
   inputs.tag.value = tag;
+  inputs.dueDate.value = new Date(dueDate).toISOString().split('T')[0];
 
   submitButton.onclick = async () => {
     if (!['high', 'medium', 'low'].includes(inputs.priority.value)) {
-      return alert('Priority must be high, medium, or low');
+      return showErrorToast('Priority must be high, medium, or low');
     }
 
     const newTask: TaskType = {
       ...task,
       description: inputs.description.value,
       priority: inputs.priority.value as 'low' | 'medium' | 'high',
-      tag,
+      dueDate: inputs.dueDate.value,
+      tag: inputs.tag.value,
     };
 
     await httpPatchOne(newTask);
