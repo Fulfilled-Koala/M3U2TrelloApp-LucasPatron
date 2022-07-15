1a8d15ce123d3b1f6ebfd59276dfffd4761b0cca
lucas <lpatronr@gmail.com>
2022-07-15 01:46:59 -0500

    refactor: remove code duplication

--- src/toasts/success.ts
+++ src/toasts/success.ts
@@ -1,19 +1,6 @@
 import { elements } from "../utils/elements";
-import { showToast } from "./utils/show-toast";
-
-const { toast, closeButton, message } = elements.toasts.success;
+import { appendEventListener } from "./utils/show-toast";
 
 export default function showSuccessToast(prompt: string) {
-  closeButton.onclick = () => {
-    toast.classList.add("-left-full", "hidden");
-    toast.classList.remove("left-0", "flex");
-  };
-
-  if (toast.classList.contains("flex")) {
-    setTimeout(() => {
-      showToast(prompt, toast, message);
-    }, 3000);
-    return;
-  }
-  showToast(prompt, toast, message);
+  appendEventListener({ ...elements.toasts.success, prompt });
 }
--- src/toasts/utils/show-toast.ts
+++ src/toasts/utils/show-toast.ts
@@ -1,4 +1,6 @@
-export function showToast(prompt: string, toast: HTMLDivElement, message: HTMLParagraphElement) {
+import { elements } from "../../utils/elements";
+
+function showToast(prompt: string, toast: HTMLDivElement, message: HTMLParagraphElement) {
   message.textContent = prompt;
   toast.classList.remove("hidden", "-left-full");
   toast.classList.add("flex", "left-0");
@@ -7,4 +9,24 @@ export function showToast(prompt: string, toast: HTMLDivElement, message: HTMLPa
     toast.classList.add("-left-full", "hidden");
     toast.classList.remove("left-0", "flex");
   }, 3000);
+}
+
+export function appendEventListener({
+  toast,
+  closeButton,
+  message,
+  prompt,
+}: typeof elements.toasts.error & { prompt: string }) {
+  closeButton.onclick = () => {
+    toast.classList.add("-left-full", "hidden");
+    toast.classList.remove("left-0", "flex");
+  };
+
+  if (toast.classList.contains("flex")) {
+    setTimeout(() => {
+      showToast(prompt, toast, message);
+    }, 3000);
+    return;
+  }
+  showToast(prompt, toast, message);
 }
